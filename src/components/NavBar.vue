10584f865755fe09b49f5fc3ec7fdf33f8bccc4e
ryanmlk <ryanmoses99@gmail.com>
2024-01-21 19:30:48 +0530

    Added intro page content

---
+++ src/components/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="navContainer">
+        <div class="item">HOME</div>
+        <div class="item">ABOUT</div>
+        <div class="item">CONTACT</div>
+    </div>
+</template>
+
+<style scoped>
+.navContainer {
+    margin-top: 20px;
+    position: fixed;
+    display: flex;
+    justify-content: flex-end;
+    flex-direction: row;
+    top: 0;
+    right: 20px;
+    width: 100vw;
+
+    .item {
+        font-family: var(--primary-font);
+        padding: 10px;
+        font-size: 1.2em;
+        color: var(--secondary-color);
+        cursor: pointer;
+        font-weight: 600;
+    }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,72 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import NavBar from '@/components/NavBar.vue';
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <NavBar/>
+    <div class="root">
+      <div class="leftSection">
+        <h2>HI I'M</h2>
+        <h1>RYAN</h1>
+        <p>Full-stack software engineer with a passion for learning and creating elegant solutions</p>
+      </div>
+      <div class="rightSection">
+        <img class="hero" src="/images/hero.png">
+      </div>
+    </div>
   </main>
 </template>
+
+<style scoped>
+  main {
+    background-color: var(--primary-color);
+    width: 100vw;
+    height: 100vh;
+  }
+
+  .root {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding: 100px;
+    color: var(--secondary-color);
+
+    .leftSection {
+      flex: 1;
+      h1 {
+        font-weight: 600;
+        margin-left: -8px;
+        line-height: 95px;
+        font-family: var(--primary-font);
+        font-size: 96px;
+      }
+      h2 {
+        font-weight: 600;
+        font-family: var(--primary-font);
+        font-size: 27px;
+      }
+      p{
+        font-weight: 600;
+        margin-top: 22px;
+        font-family: var(--secondary-font);
+        font-size: 24px;
+        width: 90%;
+        line-height: 30px;
+      }
+    }
+
+    .rightSection {
+      flex: 1;
+      display: flex;
+      align-items: center;
+
+      .hero {
+        max-width: 100%;
+        max-height: 100%;
+      }
+    }
+  }
+</style>
